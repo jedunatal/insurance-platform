2429f0bd5ab7895be7cf4259c8eb2a4e4189ab12
jedunatal <wyd.jrg@gmail.com>
2026-06-21 19:23:01 -0300

    feat: define fillable attributes and relationships for Tenant model

--- app/Models/Tenant.php
+++ app/Models/Tenant.php
@@ -3,8 +3,23 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Tenant extends Model
 {
-    //
+    protected $fillable = [
+        'name',
+        'slug',
+        'is_active',
+    ];
+
+    public function users(): HasMany
+    {
+        return $this->hasMany(User::class);
+    }
+
+    public function leads(): HasMany
+    {
+        return $this->hasMany(Lead::class);
+    }
 }
