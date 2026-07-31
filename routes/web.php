adc3673526fff4226cd3a837b0a7423385f045da
jedunatal <wyd.jrg@gmail.com>
2026-07-30 21:46:47 -0300

    feat: update sidebar navigation and routes for improved navigation structure

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,5 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('home');
-})->name('home');
-
-Route::get('/dashboard', function () {
     return view('dashboard');
 })->name('dashboard');
